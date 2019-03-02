a82383e35b301a3436c5ebad38b69bcb0b15b7c7
Robel Tekle <robel.tekle@gmail.com>
2019-03-02 17:18:02 +0100

    improve helper

--- server/router/createChatBox/helper.js
+++ server/router/createChatBox/helper.js
@@ -1,28 +1,36 @@
 const { v4 } = require('uuid')
 
 const checkBody = body => {
-  if (typeof body !== 'string') {
+  if (!body || typeof body !== 'string') {
     return {
       obj: null,
       error: true,
       message: 'No body filled',
     }
   }
-  const obj = JSON.parse(body)
-  const keys = Object.keys(obj)
+  try {
+    const obj = JSON.parse(body)
+    const keys = Object.keys(obj)
 
-  if (keys.length > 1 || !keys.includes('name') || !obj.name) {
+    if (keys.length > 1 || !keys.includes('name') || !obj.name) {
+      return {
+        obj: null,
+        error: true,
+        message: 'No valid fields filled. Required field: name',
+      }
+    }
+
+    return {
+      obj: { id: v4(), name: obj.name, messagesId: [] },
+      error: false,
+      message: null,
+    }
+  } catch (err) {
     return {
       obj: null,
       error: true,
-      message: 'No valid fields filled. Required field: name',
+      message: 'No valid JSON filled',
     }
-  }
-
-  return {
-    obj: { id: v4(), name: obj.name, messagesId: [] },
-    error: false,
-    message: null,
   }
 }
 
